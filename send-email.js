1fb65de298cf10595cce6c191ac40916d61ad722
spenserlogan <spenserlogan@gmail.com>
2021-05-08 11:29:24 -0500

    nodemailer model

---
+++ send-email.js
@@ -0,0 +1,24 @@
+const nodemailer = reqiure('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    service: '',
+    auth: {
+        user: '',
+        password: ''
+    }
+});
+
+let mailOption = {
+    from: '',
+    to: '',
+    subject: '',
+    text: ``,
+};
+
+transporter.sendMail(mailOption, function (err, info) {
+    if(err) {
+        console.log(err);
+    }else {
+        console.log('Email sent: ' + info.response)
+    }
+});
