0af0ff521099bd374f3f59bbff0ff561a3938f97
spenserlogan <spenserlogan@gmail.com>
2021-05-08 13:30:30 -0500

    got email to send

--- send-email.js
+++ send-email.js
@@ -1,24 +1,28 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
+// const {User} = require('./models/User');
+// const bcrypt = require('bcrypt');
 
 let transporter = nodemailer.createTransport({
-    service: '',
+    service: 'gmail',
     auth: {
-        user: '',
-        password: ''
+        // model: User,
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
 });
 
 let mailOption = {
-    from: '',
-    to: '',
-    subject: '',
-    text: ``,
+    from: 'bill.reminder.project@gmail.com',
+    to: 'samuel6roth@gmail.com',
+    subject: 'testing',
+    text: `Is this working?`,
 };
 
-transporter.sendMail(mailOption, function (err, info) {
+transporter.sendMail(mailOption, function (err, data) {
     if(err) {
         console.log(err);
     }else {
-        console.log('Email sent: ' + info.response)
+        console.log('Email sent: ' + data.response)
     }
 });
