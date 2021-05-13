6dba4f63cd74a45cd2ff2385c16aa1a9f46f92f8
Kyle Rather <krather7@gmail.com>
2021-05-13 18:45:17 -0500

    send email.js updated

--- controllers/api/send-email.js
+++ controllers/api/send-email.js
@@ -1,70 +1,37 @@
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
-//require('dotenv').config();
-//const {User} = require('../../models');
-//const bcrypt = require('bcrypt');
-
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.PASSWORD
-//     }
-// });
-
+require('dotenv').config();
+const { User } = require('../../models');
 // const sendEmail = async () => {
 //     const userRaw = await User.findByPk(4)
 //     const user = userRaw.get({ plain: true })
 //     console.log('>>> user data : ', user);
-
 // let mailOption = {
 //     from: 'bill.reminder.project@gmail.com',
 //     to: [user.email],
 //     subject: 'Upcoming Bill Due!',
 //     text: `Is this working?`,
 // };
-
-//     cron.schedule('* * 8, * sunday', () => {
-
-//         transporter.sendMail(mailOption, function (err, data) {
-//             if(err) {
-//                 console.log(err);
-//             }else {
-//                 console.log('Email sent: ' + data.response)
-//             }
-//         });
-//     });
-// }
-// // sendEmail();
-// // const emailselector = async () => {
-    
-// // }
-
-// module.exports = sendEmail;
-
-
-
-
 const emailSender = async () => {
-
+    const userRaw = await User.findByPk(2)
+    const user = userRaw.get({ plain: true })
+    console.log('>>> user data : ', user);
     let transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: "bill.reminder.project@gmail.com",
-            pass: "fleetwoodmac"
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
     })
-
     cron.schedule("*/10 * * * * *", () => {
         console.log("sending email")
         let mailOptions = {
             from: "bill.reminder.project@gmail.com",
-            to: "samuel6roth@gmail.com",
+            to: [user.email],
             subject: "Nodemailer",
             text: "Testing Nodemailer",
             html: "<h1>Testing Nodemailer</h1>"
     }
-
     transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
             console.log("error occurred", err)
@@ -74,5 +41,4 @@ const emailSender = async () => {
     })
     })
 }
-
 module.exports = emailSender;
