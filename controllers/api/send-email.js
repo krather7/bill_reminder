22199cfe37c464b16914621d63e461a29c34794c
Kyle <florez.photo@gmail.com>
2021-05-15 11:57:23 -0500

    fixed email sender

--- controllers/api/send-email.js
+++ controllers/api/send-email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
 require('dotenv').config();
-const { User } = require('../../models');
+const { User, Bill } = require('../../models');
 
 // THIS CODE WORKS //
 // const userRaw = await User.findByPk(1)
@@ -11,9 +11,17 @@ const { User } = require('../../models');
 
 const emailSender = async () => {
 
-    const users = await User.findAll();
-    const allUsers = (JSON.stringify(users, null, 2));
-    console.log("All users:", allUsers)
+    const usersRaw = await User.findAll({
+        attributes: ['email', 'name'],
+        include: [{model: Bill}]
+    });
+    const users = usersRaw.map( u => u.get({plain: true}))
+    // const allUsers = (JSON.stringify(users.map((u) =>{
+    //     return u.email;
+    //     }), null, 2));
+    //console.log("All users:", JSON.stringify(users, null, 2))
+
+
 
     let transporter = nodemailer.createTransport({
         service: "gmail",
@@ -23,21 +31,39 @@ const emailSender = async () => {
         }
     })
     cron.schedule("*/10 * * * * *", () => {
-        console.log("sending email")
-        let mailOptions = {
-            from: "bill.reminder.project@gmail.com",
-            to: allUsers,
-            subject: "Nodemailer",
-            text: "Testing Nodemailer",
-            html: "<h1>Testing Nodemailer</h1>"
-    }
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.log("error occurred", err)
-        } else {
-            console.log("email sent", info)
-        }
-    })
+        //console.log("sending email", allUsers);
+
+        users.forEach( async (u) => {
+            let mailOptions = {
+                from: "bill.reminder.project@gmail.com",
+                to: u.email,
+                subject: "Bill Remainder",
+                text: `Hi ${u.name},\nYou have a bill to pay!`,
+                html: `<h1>Hey ${u.name},\nYou got ${u.bills.length} bill(s).</h1><br><p>${u.bills.map( b => b.name + ', ')}</p>`
+            }
+            console.log('>>>>>', mailOptions)
+            await transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    console.log("error occurred", err)
+                } else {
+                    console.log("email sent", info)
+                }
+            })
+        })
+    //     let mailOptions = {
+    //         from: "bill.reminder.project@gmail.com",
+    //         to: allUsers,
+    //         subject: "Nodemailer",
+    //         text: "Testing Nodemailer",
+    //         html: "<h1>Testing Nodemailer</h1>"
+    // }
+    // transporter.sendMail(mailOptions, (err, info) => {
+    //     if (err) {
+    //         console.log("error occurred", err)
+    //     } else {
+    //         console.log("email sent", info)
+    //     }
+    // })
     })
 }
 
